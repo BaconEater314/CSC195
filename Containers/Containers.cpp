fed86335fbf03be589bdeb5bb911576304646b29
BaconEater314 <nickbeaty314@gmail.com>
2025-04-08 14:07:59 -0600

    Work for containers assignment

--- Containers/Containers.cpp
+++ Containers/Containers.cpp
@@ -11,34 +11,54 @@ using namespace std;
 int main() {
 
 	// array
-	array<int, 5> arr = {1,2,3,4,5};
-	cout << arr.size() << endl;
+	array<string,7> arr = {"Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"};
 
 	for (int i = 0; i < arr.size(); i++) {
 		cout << arr[i] << endl;
 	}
 
+	cout << " " << endl;
 	//vector
 	vector<int> vec = { 1,2,3,4,5 };
 	vec.push_back(6);
+	vec.push_back(7);
+	vec.pop_back();
 
-	cout << vec.size() << endl;
-	cout << vec.capacity() << endl;
+	for (int i = 0; i < vec.size(); i++) {
+		cout << vec[i] << endl;
+	}
 
+	cout << " " << endl;
 	//list
-	list<int> lst = { 1,2,3,4,5 };
-	lst.push_back(6);
-	lst.insert(lst.begin(), 0);
+	list<string> lst = {"Apple", "Banana", "Orange"};
+	lst.insert(lst.begin(), "Strawberry");
+	lst.insert(lst.end(), "Blueberry");
+	lst.remove("Orange");
 
+	for (auto iter = lst.begin(); iter != lst.end(); iter++) {
+		cout << *iter << endl;
+	}
+
+	cout << " " << endl;
 	//map
 	map<string, int> m;
-	m["riley"] = 46;
-	m["garret"] = 81;
-	m["mike"] = 100;
+	m["apples"] = 10;
+	m["bananas"] = 5;
+	
+	m["oranges"] = 10;
+	m["strawberries"] = 15;
+	m["blueberries"] = 20;
 
-	cout << m["riley"] << endl;
+	for (const auto& pair : m) {
+		std::cout << pair.first << ": " << pair.second << std::endl;
+	}
 
 	//stack
+	stack<int> s;
+	s.push(1);
+	s.push(2);
+	s.push(3);
+	s.pop();
 
 	return 0;
  }
